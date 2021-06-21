ff1419206091b2affa7a201b6040965c2cd0398b
acetyl-CoA <ori844acul@naver.com>
2021-06-21 22:48:12 +0900

    create final application methods

---
+++ DaengDaengDogam/DDDG/app/src/main/java/com/example/dddg/APIKEYS.java
@@ -0,0 +1,8 @@
+package com.example.dddg;
+
+public class APIKEYS {
+    String naverclientid = "6mL5T1oSy_X8fV89_VVv";
+    String naverclientsecret = "COilFLmROO";
+    String publickey = "=SyP6F9ejcqriDqKdjU7q7CxNczUmORPp81PQ2GVXzLI%2FUuLHYB%2FSalI7KHGDqWitbyHbmAbZweKcsETIXsTgCQ%3D%3D";
+    String youtubekey = "AIzaSyAnwRXN9oNhnjVObaeRqAG0A6H3h4B7J8I";
+}
